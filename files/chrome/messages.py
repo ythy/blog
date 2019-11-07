79cee72d60d9c832fd1f62917130317dde08a0ac
ythy <ythyhvh@163.com>
2019-11-07 14:50:28 +0800

    Create messages.py

---
+++ files/chrome/messages.py
@@ -0,0 +1,80 @@
+from tkinter import messagebox, Tk
+import sys
+import json
+import struct
+
+
+def alert(title, message, kind='info', hidemain=True):
+    if kind not in ('error', 'warning', 'info'):
+        raise ValueError('Unsupported alert kind.')
+
+    show_method = getattr(messagebox, 'show{}'.format(kind))
+    show_method(title, message)
+
+
+# if __name__ == '__main__':
+#     Tk().withdraw()
+#     alert('Hello', 'Hello World')
+#     alert('Hello Again', 'Hello World 2', kind='warning')
+
+try:
+    # Python 3.x version
+    # Read a message from stdin and decode it.
+    def getMessage():
+        rawLength = sys.stdin.buffer.read(4)
+        if len(rawLength) == 0:
+            sys.exit(0)
+        messageLength = struct.unpack('@I', rawLength)[0]
+        message = sys.stdin.buffer.read(messageLength).decode('utf-8')
+        return json.loads(message)
+
+    # Encode a message for transmission,
+    # given its content.
+    def encodeMessage(messageContent):
+        encodedContent = json.dumps(messageContent).encode('utf-8')
+        encodedLength = struct.pack('@I', len(encodedContent))
+        return {'length': encodedLength, 'content': encodedContent}
+
+    # Send an encoded message to stdout
+    def sendMessage(encodedMessage):
+        sys.stdout.buffer.write(encodedMessage['length'])
+        sys.stdout.buffer.write(encodedMessage['content'])
+        sys.stdout.buffer.flush()
+
+    while True:
+        receivedMessage = getMessage()
+        if receivedMessage == "ping":
+            sendMessage(encodeMessage("pong3"))
+        root = Tk()
+        root.wm_attributes('-topmost',1)
+        root.withdraw()
+        alert('chat room', receivedMessage)
+except AttributeError:
+    # Python 2.x version (if sys.stdin.buffer is not defined)
+    # Read a message from stdin and decode it.
+    def getMessage():
+        rawLength = sys.stdin.read(4)
+        if len(rawLength) == 0:
+            sys.exit(0)
+        messageLength = struct.unpack('@I', rawLength)[0]
+        message = sys.stdin.read(messageLength)
+        return json.loads(message)
+
+    # Encode a message for transmission,
+    # given its content.
+    def encodeMessage(messageContent):
+        encodedContent = json.dumps(messageContent)
+        encodedLength = struct.pack('@I', len(encodedContent))
+        return {'length': encodedLength, 'content': encodedContent}
+
+    # Send an encoded message to stdout
+    def sendMessage(encodedMessage):
+        sys.stdout.write(encodedMessage['length'])
+        sys.stdout.write(encodedMessage['content'])
+        sys.stdout.flush()
+
+    while True:
+        receivedMessage = getMessage()
+        if receivedMessage == "ping":
+            sendMessage(encodeMessage("pong2"))
+
